4d4322f9790f0c6f5b850416443ecbd0e2a38b51
bala-amypo <bala@amypo.in>
2025-12-23 08:46:47 +0000

    User commit

--- src/main/java/com/example/demo/entity/AcademicEventEntity.java
+++ src/main/java/com/example/demo/entity/AcademicEventEntity.java
@@ -25,7 +25,7 @@ public class AcademicEventEntity {
         submittedAt = LocalDateTime.now();
     }
 
-    // getters/setters
+
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
