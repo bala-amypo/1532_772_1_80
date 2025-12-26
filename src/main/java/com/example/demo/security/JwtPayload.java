3a6f327faa2063640897a0325896bf3bf8859cbd
bala-amypo <bala@amypo.in>
2025-12-26 10:49:24 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtPayload.java
+++ src/main/java/com/example/demo/security/JwtPayload.java
@@ -1,17 +1,17 @@
-package com.example.demo.security;
+// package com.example.demo.security;
 
-import io.jsonwebtoken.Claims;
+// import io.jsonwebtoken.Claims;
 
-public class JwtPayload {
+// public class JwtPayload {
 
-    private final Claims claims;
+//     private final Claims claims;
 
-    public JwtPayload(Claims claims) {
-        this.claims = claims;
-    }
+//     public JwtPayload(Claims claims) {
+//         this.claims = claims;
+//     }
 
-    // 🔥 TEST EXPECTS THIS METHOD
-    public Claims getPayload() {
-        return claims;
-    }
-}
+//     // 🔥 TEST EXPECTS THIS METHOD
+//     public Claims getPayload() {
+//         return claims;
+//     }
+// }
